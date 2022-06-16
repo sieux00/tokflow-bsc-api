46eac79b0052bf43b272ca7d711def770af84ef5
sieux00 <39194150+sieux00@users.noreply.github.com>
2022-06-16 18:14:59 +0700

    update

--- routers/Chart.js
+++ routers/Chart.js
@@ -10,6 +10,8 @@ const Chart = {
         //#region GET 
         app.get('/getChart/:token',
             this.getChart);
+        app.get('/getInfoTokens',
+            this.getInfoTokens);
         //#endregion
 
         //#region POST 
@@ -18,34 +20,41 @@ const Chart = {
 
     //#region GET 
     async getChart(req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(500).send({ success: false, msg: errors.array()[0].msg });
-        }
-
         var bars = []
-
-        for (let i = 1629331200 - 1000000; i <= 1655337600 + 1000000; i = i + 60) {
+        console.log(req.query)
+        var from = parseInt(req.query.from)
+        var to = parseInt(req.query.to)
+        //{ from: '1655345148', to: '1655363148' }
+        for (let i = from; i <= to; i = i + 60) {
             const bar = {
                 time: i * 1000,
-                open: getRandomInt(1000, 100000),
-                high: getRandomInt(1000, 100000),
-                low: getRandomInt(1000, 100000),
-                close: getRandomInt(1000, 100000),
+                open: getRandomInt(100, 100000),
+                high: getRandomInt(100, 100000),
+                low: getRandomInt(100, 100000),
+                close: getRandomInt(100, 100000),
                 isBarClosed: true,
                 isLastBar: false,
             }
             bars.push(bar)
         }
 
-        bars[bars.length - 1].isBarClosed = false
-        bars[bars.length - 1].isLastBar = true
+        if (req.query.resolution === '1D') {
+            bars = processTokenDay(bars)
+        }
+
+        // bars[bars.length - 1].isBarClosed = false
+        // bars[bars.length - 1].isLastBar = true
 
         console.log(bars.length)
         console.log(req.params.token)
 
-        //var bars = await DatabaseService.getChart(db, req.params.token)
+        //var bars = await DatabaseService.getChart(db, req.params.token, from, to)
         return res.status(200).send(bars)
+    },
+
+    async getInfoTokens(req, res) {
+        var infoTokens = await DatabaseService.getInfoTokens(db)
+        return res.status(200).send(infoTokens)
     }
     //#endregion
 
@@ -56,6 +65,44 @@ const Chart = {
 // random interger from 1000 to 100000
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function processTokenDay(bars) {
+    console.log("processTokenDay")
+    console.log(bars.length)
+    var openTime = bars[0].time
+    var closeTime = openTime + 24 * 60 * 60 * 1000
+    var listBarProcess = []
+    var result = []
+
+    for (let i = 0; i < bars.length; i++) {
+        let bar = bars[i]
+
+        if (bar.time <= closeTime) {
+            listBarProcess.push(bar)
+        } else {
+            let dayBar = processRage(listBarProcess)
+            result.push(dayBar)
+            openTime = bar.time
+            closeTime = openTime + 24 * 60 * 60 * 1000
+            listBarProcess = []
+            listBarProcess.push(bar)
+        }
+    }
+
+    return result
+}
+
+function processRage(listBarProcess) {
+    return bar = {
+        time: listBarProcess[0].time,
+        open: listBarProcess[0].open,
+        high: Math.max.apply(null, listBarProcess.map(item => item.high)),
+        low: Math.min.apply(null, listBarProcess.map(item => item.low)),
+        close: listBarProcess[listBarProcess.length - 1].close,
+        isBarClosed: true,
+        isLastBar: false,
+    }
 }
 
 
--- services/DatabaseService.js
+++ services/DatabaseService.js
@@ -1,19 +1,29 @@
 require('dotenv').config({ path: '../.env' });
 
 const DatabaseService = {
-    getChart(dbo, token) {
+    getChart(dbo, token, from, to) {
         return new Promise((resolve, reject) => {
             dbo.collection("prices_minutes")
-                .find({ token })
+                .find({ token, openTime: { $gte: from, $lte: to } })
                 .sort({ openTime: 1 })
+                .limit(2000)
                 .toArray((err, docs) => {
                     if (err) return resolve([])
                     resolve(docs)
                 })
         })
-
-
     },
+
+    getInfoTokens(dbo) {
+        return new Promise((resolve, reject) => {
+            dbo.collection("tokens")
+                .find({})
+                .toArray((err, docs) => {
+                    if (err) return resolve([])
+                    resolve(docs)
+                })
+        })
+    }
 }
 
 module.exports = DatabaseService
